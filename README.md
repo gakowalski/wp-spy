87951c01c97db349581dfae8da6ff65f31a802f7
Grzegorz Kowalski <grzegorz.adam.kowalski@outlook.com>
2023-09-20 10:58:59 +0200

    Analyze api namespaces and post types

--- README.md
+++ README.md
@@ -2,15 +2,17 @@
 
 ## Description
 
-Two simple scripts:
+Three simple scripts:
 1. Enumerate all WP posts by their ID and get canonical links.
 2. Get all authors from Wordpress REST API
+3. Guess plugins from REST API endpoints and scan for available post types
 
 ## Usage
 
 ```bash
 php spy.php
 php spy-users.php
+php spy-api.php
 ```
 
 or
---
+++ spy-api.php
@@ -0,0 +1,139 @@
+<?php
+
+// if first argument does not contain target URL, ask for it and read it from stdin
+if (empty($argv[1])) {
+    echo "Enter target URL: ";
+    $target = trim(fgets(STDIN));
+} else {
+    $target = $argv[1];
+}
+
+// if target URL does not contain http:// or https://, add https://
+if (strpos($target, 'http://') === false && strpos($target, 'https://') === false) {
+    $target = 'https://' . $target;
+}
+
+// if target URL does not end with /, add it
+if (substr($target, -1) != '/') {
+    $target .= '/';
+}
+
+$api_url = $target . 'wp-json/';
+$api_json = file_get_contents($api_url);
+
+// if no JSON data found, exit
+if (empty($api_json)) {
+    echo "No JSON data found\n";
+    exit;
+}
+
+// decode JSON data
+$api_data = json_decode($api_json, true);
+$namespaces = $api_data['namespaces'];
+
+$namepsace_to_plugin_name = [
+    'ssp' => 'Seriously Simple Podcasting',
+    'wordfence' => 'Wordfence',
+    'yoast' => 'Yoast SEO',
+    'divi' => 'Divi',
+    'uag' => 'Ultimate Addons for Gutenberg (UAG, now Spectra)',
+    'spectra' => 'Spectra - WordPress Gutenberg Blocks',
+    'elementor' => 'Elementor',
+    'elementor-pro' => 'Elementor Pro',
+    'wp' => 'WordPress REST API',
+    'otter' => 'Otter Blocks',
+    'wp-site-health' => 'Wordpress Site Health',
+    'wp-block-editor' => 'Wordpress Block Editor (Gutenberg)',
+    'oembed' => 'Wordpress oEmbed Provider API',
+    'litespeed' => 'LiteSpeed Cache',
+    'simplystatic' => 'Simply Static',
+    'google-site-kit' => 'Google Site Kit',
+    'filebird' => 'FileBird - WordPress Media Library Folders & File Manager',
+    'SBR' => 'Simple Basic Registration',
+    'wp-super-cache' => 'WP Super Cache',
+    'contact-form-7' => 'Contact Form 7',
+    'duplicator' => 'Duplicator',
+    'redirection' => 'Redirection',
+];
+
+fputcsv(STDOUT, ['Type', 'Name', 'Namespace', 'Version / Count', 'Schema']);
+foreach ($namespaces as $namespace) {
+    // split namespace into parts, eg. oembed/1.0 -> oembed, 1.0
+    $namespace_parts = explode('/', $namespace);
+    $namespace_name = $namespace_parts[0];
+    $namespace_version = $namespace_parts[1];
+
+    // get plugin name from $namepsace_to_plugin_name if exists, otherwise Unknown
+    if (!isset($namepsace_to_plugin_name[$namespace_name])) {
+        $plugin_name = 'Unknown';
+    } else {
+        $plugin_name = $namepsace_to_plugin_name[$namespace_name];
+    }
+
+    fputcsv(STDOUT, ['Plugin', $plugin_name, $namespace_name, $namespace_version, $api_url . $namespace]);
+}
+
+$api_json = file_get_contents($api_url . 'wp/v2/types');
+$api_data = json_decode($api_json, true);
+
+$types = $api_data;
+
+$type_to_type_name = [
+    'post' => 'Post',
+    'page' => 'Page',
+    'attachment' => 'Attachment',
+    'nav_menu_item' => 'Menu Item',
+    'wp_block' => 'Block',
+    'wp_template' => 'Template',
+    'wp_template_part' => 'Template Part',
+    'wp_navigation' => 'Navigation',
+    'spectra-popup' => 'Spectra Popup',
+];
+
+$types_for_logged_users = [
+    'attachment',
+    'wp_template',
+    'wp_template_part',
+    'nav_menu_item',
+];
+
+//fputcsv(STDOUT, ['Type', 'Name', 'Namespace', 'Version', 'Schema']);
+
+foreach ($types as $type) {
+    $type_name = $type['name'];
+    $type_slug = $type['slug'];
+    $rest_namespace = $type['rest_namespace'];
+    $rest_base = $type['rest_base'];
+    $rest_url = "$api_url$rest_namespace/$rest_base";
+
+    // get type name from $type_to_type_name if exists, otherwise Unknown
+    if (!isset($type_to_type_name[$type_slug])) {
+        $type_name_2 = 'Unknown';
+    } else {
+        $type_name_2 = $type_to_type_name[$type_slug];
+    }
+
+    // if type is $types_for_logged_users
+    if (!in_array($type_slug, $types_for_logged_users)) {
+        // get JSON contents from $rest_url
+        $rest_json = file_get_contents($rest_url);
+
+        // decode JSON data
+        $rest_data = json_decode($rest_json, true);
+
+        // if JSON data found, check if it is an array and count the number of elements
+        if (!empty($rest_data)) {
+            if (is_array($rest_data)) {
+                $rest_count = count($rest_data);
+            } else {
+                $rest_count = 1;
+            }
+        } else {
+            $rest_count = 0;
+        }
+    } else {
+        $rest_count = 'N/A';
+    }
+
+    fputcsv(STDOUT, ['Type', "$type_name ($type_name_2)", $type_slug, $rest_count, $rest_url]);
+}
